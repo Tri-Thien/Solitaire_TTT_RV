ecd21e9b500cc41ee4fc9c01ebaabe0f8ca9fd51
E164708F <nostale789>
2017-11-24 13:56:05 +0100

    Modification de vue

--- vue/vue.php
+++ vue/vue.php
@@ -15,46 +15,8 @@ header("Content-type: text/html; charset=utf-8");
 </head>
 <body>
 
-
-
-<br/>
-<br/>
-<form method="post" action="index.php">
-Entrer votre pseudo  <input type="text" name="pseudo"/>
-</br>
-</br>
-<input type="submit" name="soumettre" value="envoyer"/>
-</form>
-<br/>
-<p> BONJOUR </p>
-<br/>
-<?php
-}
-
-function salon($messages){
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8" />
-	<title>Salon</title>
-</head>
-<body>
-<br/>
-<h1>Bienvenue dans le salon <?php echo $_SESSION["pseudo"]?></h1>
-<br/>
-<form method="post" action="index.php">
-Entrez votre message <input type="text" name="message"/>
-<input type="submit" name="soumettre" value="envoyer"/>
-</form>
-<br/>
-<?php
-foreach ($messages as $tab){
-echo $tab->pseudo." -> ".$tab->message."<br/>";
-}
-?>
-<br/>
-<a href="index.php">Se déconnecter</a>
+</body>
+</html>
 <?php
 }
 
